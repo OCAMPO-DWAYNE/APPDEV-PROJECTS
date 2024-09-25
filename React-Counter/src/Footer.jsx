3a92cefb904556d3f9234a78d24f7c9052e6f898
OCAMPO-DWAYNE <20207223@s.ubaguio.edu>
2024-09-25 16:33:10 +0800

    Add files via upload

---
+++ React-Counter/src/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer(){
+    return(
+        
+        <footer>
+            <hr>
+            </hr>
+            <p>
+                &copy; {new Date().getFullYear()} My Counter Website ||
+                Written by: Ocampo, Dwayne John
+            </p>
+        </footer>
+              
+    )
+}
